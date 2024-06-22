5f5fd9956a656bea1123535efc77ad51b3079ff7
pablotebb <4356464+pablotebb@users.noreply.github.com>
2024-06-22 17:13:53 +0100

    6. Clase 2.2

--- aplicacionFlask/app.py
+++ aplicacionFlask/app.py
@@ -1,4 +1,3 @@
-# Además de Flask, importamos la función render_template de flask.
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,13 +5,17 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     nombre = "Pablo"
-    # En lugar de devolver texto simple, usamos render_template para renderizar los archivos HTML y pasamos la variable nombre a las plantillas.
     return render_template('index.html', nombre=nombre)
 
 @app.route('/about')
 def about():
     nombre = "Pablo"
     return render_template('about.html', nombre=nombre)
+  
+@app.route('/contact')
+def contact():
+    nombre = "Pablo"
+    return render_template('contact.html', nombre=nombre)
 
 if __name__ == '__main__':
     app.run(debug=True)
